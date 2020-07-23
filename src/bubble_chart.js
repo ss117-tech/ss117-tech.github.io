494b2a8b6af5145d57616315a36ba6d273d6bf0c
c3subramanianshankar <32652945+c3subramanianshankar@users.noreply.github.com>
2020-07-23 05:26:44 -0700

    Test11

--- src/bubble_chart.js
+++ src/bubble_chart.js
@@ -19,16 +19,16 @@ function bubbleChart() {
   var center = { x: width / 2, y: height / 2 };
 
   var yearCenters = {
-    2008: { x: width / 3, y: height / 2 },
-    2009: { x: width / 2, y: height / 2 },
-    2010: { x: 2 * width / 3, y: height / 2 }
+    Canada: { x: width / 3, y: height / 2 },
+    France: { x: width / 2, y: height / 2 },
+    China: { x: 2 * width / 3, y: height / 2 }
   };
 
   // X locations of the year titles.
   var yearsTitleX = {
-    2008: 160,
-    2009: width / 2,
-    2010: width - 160
+    Canada: 160,
+    France: width / 2,
+    China: width - 160
   };
 
   // @v4 strength to apply to the position forces
