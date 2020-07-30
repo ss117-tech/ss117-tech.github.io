a4033a511614e68603915fbfe67a300abcf04c9a
c3subramanianshankar <32652945+c3subramanianshankar@users.noreply.github.com>
2020-07-30 02:51:43 -0700

    Ccode change

--- airports.js
+++ airports.js
@@ -136,7 +136,7 @@
     function displayMap(countries){
 
 
-      //dataSets.mapProjection = d3.geoMercator().scale(97).translate([width / 2, height / 2 + 20]);
+
 
       var container = d3.select("#map");
       container.attr("width", width).attr("height", height)
@@ -144,23 +144,22 @@
 
 
 
-      let path = d3.geoPath().projection(projection) //TODO: create a geoPath generator and set its projection to be the projection passed as parameter.
 
-      container.selectAll("path").data(countries.features)
-      .enter().append("path")
-      .attr("d",d => path(d)) //TODO: use the path generator to draw each country )
-      .attr("stroke", "#ccc")
-      .attr("fill", "#eee")
     }
 
-    function displayAirports(airports) {
-      //let config = getMapConfig(); //get the config
-      //let projection = getMapProjection(config) //get the projection
+    function displayAirports(airports, countries) {
 
-      container = d3.select("#map");//TODO: select the svg with id Map
+
+      container = d3.select("#map");
+      var path = d3.geoPath().projection(projection)
+
       container.attr("width", width).attr("height", height)
+      .selectAll("path").data(countries.features)
+      .enter().append("path")
+      .attr("d",d => path(d))
+      .attr("stroke", "#ccc")
+      .attr("fill", "#eee")
 
-      //let container = config.container; //get the container
 
       container.attr("width", width).attr("height", height)
       .selectAll("circle")
@@ -173,40 +172,38 @@
     }
 
     function airportGrouping(data) {
-        //We use reduce to transform a list into a object where each key points to an aiport. This way makes it easy to check if is the first time we are seeing the airport.
-        let airlineDict = data.reduce((airlineDict, d) => {
-            //The || sign in the line below means that in case the first option is anything that Javascript consider false (this insclude undefined, null and 0), the second option will be used. Here if airlineDict[d.DestinationAirportID] is false, it means that this is the first time we are seeing the airport, so we will create a new one (second part after ||)
+        var airlineDict = data.reduce((airlineDict, d) => {
 
-            let currentDest = airlineDict[d.DestinationAirportID] || {
+            var presentDest = airlineDict[d.DestinationAirportID] || {
                 "AirportID": d.DestinationAirportID,
                 "Airport": d.DestinationAirport,
                 "Latitude": +d.DestinationLatitude,
                 "Longitude": +d.DestinationLongitude,
                 "City": d.DestinationCity,
                 "Country": d.DestinationCountry,
-                "Count": 0
+                "numberOfFlights": 0
             }
-            currentDest.numberOfFlights += 1
-            airlineDict[d.DestinationAirportID] = currentDest
 
-            //After doing for the destination airport, we also update the airport the airplane is departing from.
-            let currentSource = airlineDict[d.SourceAirportID] || {
+            presentDest.numberOfFlights += 1
+            airlineDict[d.DestinationAirportID] = presentDest
+
+            var presentSource = airlineDict[d.SourceAirportID] || {
                 "AirportID": d.SourceAirportID,
                 "Airport": d.SourceAirport,
                 "Latitude": +d.SourceLatitude,
                 "Longitude": +d.SourceLongitude,
                 "City": d.SourceCity,
                 "Country": d.SourceCountry,
-                "Count": 0
+                "numberOfFlights": 0
             }
-            currentSource.numberOfFlights += 1
-            airlineDict[d.SourceAirportID] = currentSource
+            presentSource.numberOfFlights += 1
+            airlineDict[d.SourceAirportID] = presentSource
 
             return airlineDict
         }, {})
 
-        //We map the keys to the actual ariorts, this is an way to transform the object we got in the previous step into a list.
-        airlineDict = Object.keys(airlineDict).map(key => airlineDict[key])
+        airlineKeys= Object.keys(airlineDict)
+        airlineDict = airlineKeys.map(key => airlineDict[key])
         return airlineDict
     }
 
@@ -215,14 +212,10 @@
 
     function displayAll() {
 
-      //var flightpaths = dataSets.flightpaths
+      displayBars(airlineGrouping(dataSets.flightpaths))
+      //displayMap(dataSets.geo)
 
-      //var airlines = airlineGrouping(dataSets.flightpaths); // Grouping flightpaths by airlines.
-
-      displayBars(airlineGrouping(dataSets.flightpaths)) // Draw the Airlines Chart after grouping flightpaths by airlines.
-      displayMap(dataSets.geo)
-
-      displayAirports(airportGrouping(dataSets.flightpaths))
+      displayAirports(airportGrouping(dataSets.flightpaths),dataSets.geo)
 
       displayFlightPaths("24")
     }
